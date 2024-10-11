3dcdfd9c0953ec1281fc6fce38320a4c5e170e52
Mohith-QB <mohith.nagirimadugu@qburst.com>
2024-10-11 10:07:43 +0530

    Add files via upload

---
+++ Calci/src/test/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		        // Hardcoded input values
+		        double num1 = 10;
+		        double num2 = 5;
+
+		        // Perform basic arithmetic operations
+		        double sum = num1 + num2;
+		        double difference = num1 - num2;
+		        double product = num1 * num2;
+		        double quotient = num1 / num2;
+
+		        // Display the results
+		        System.out.println("Numbers: " + num1 + " and " + num2);
+		        System.out.println("Addition: " + sum);
+		        System.out.println("Subtraction: " + difference);
+		        System.out.println("Multiplication: " + product);
+		        System.out.println("Division: " + quotient);
+		    }
+		}
